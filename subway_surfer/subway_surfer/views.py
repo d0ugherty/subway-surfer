f3014d9599f5fb267bbb4d3be729defb787e0f9b
d0ugherty <tdougherty16@gmail.com>
2024-01-09 20:52:29 -0500

    changed update_arrivals_table to retrieve table data according to a tabl_id argument

--- subway_surfer/subway_surfer/views.py
+++ subway_surfer/subway_surfer/views.py
@@ -71,25 +71,39 @@ def load_arrivals(request, station):
 
 
 """Update Arrivals"""
-def update_arrivals_table(request):
-    print(request.method)
+def update_arrivals_table(request, table_id):
     station = request.POST.get('station', "30th Street Station") 
     arrival_context = get_arrivals(station)
    # print(f'FROM UPDATE: {arrival_context}')
-    html = render_to_string('info_board/table_rows.html', {
-        'arrivals': arrival_context['all_arrivals_ctx'],
-        'air_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Airport'],
-        'che_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Chestnut Hill East'],
-        'chw_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Chestnut Hill West'],
-        'lan_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Lansdale/Doylestown'],
-        'med_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Media/Wawa'],
-        'fox_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Fox Chase'],
-        'nor_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Manayunk/Norristown'],
-        'pao_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Paoli/Thorndale'],
-        'cyn_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Cynwyd'],
-        'tre_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Trenton'],
-        'war_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Warminster'],
-        'wil_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['Wilmington/Newark'],
-        'wtr_arrivals_ctx': arrival_context['arrivals_by_line_ctx']['West Trenton'],
-        'station': station,})
+    match table_id:
+        case 'tbl_all_arrivals':
+            data = arrival_context['all_arrivals_ctx']
+        case 'tbl_air_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Airport']
+        case 'tbl_che_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Chestnut Hill East']
+        case 'tbl_chw_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Chestnut Hill West']
+        case 'tbl_lan_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Lansdale/Doylesdown']
+        case 'tbl_med_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Media/Wawa']
+        case 'tbl_fox_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Fox Chase']
+        case 'tbl_nor_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Manayunk/Norristown']
+        case 'tbl_pao_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Paoli/Thorndale']
+        case 'tbl_cyn_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Cynwyd']
+        case 'tbl_tre_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Trenton']
+        case 'tbl_war_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Warminster']
+        case 'tbl_wil_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['Wilmington/Newark']
+        case 'tnl_wtr_arrivals':
+            data = arrival_context['arrivals_by_line_ctx']['West Trenton']
+                
+    html = render_to_string('info_board/table_rows.html', {'arrivals' : data})
     return JsonResponse({'html': html})
