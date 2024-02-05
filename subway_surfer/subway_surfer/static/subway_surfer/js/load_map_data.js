9d2342f859318a6a3128b5d5d94511533aecb62b
d0ugherty <tdougherty16@gmail.com>
2024-02-04 22:58:58 -0500

    working on displaying location

--- subway_surfer/subway_surfer/static/subway_surfer/js/load_map_data.js
+++ subway_surfer/subway_surfer/static/subway_surfer/js/load_map_data.js
@@ -3,6 +3,27 @@
 function load_loc_data(element_id){
     document.addEventListener('DOMContentLoaded', function() {
       var trainLocData = JSON.parse(document.getElementById(element_id).textContent);
-      console.log(trainLocData);
-  });
+      //return trainLocData;
+      displayLocation(trainLocData);
+    });
+}
+
+function displayLocation(data){
+  data.forEach(element => console.log(element));
+}
+
+function displayTrainCurrentLoc(item, trainLayer, icon) {
+  let trainNumber = item.trainno;
+  let trainMarker = L.marker([item.lat, item.lon], {
+      icon: icon
+  }).addTo(trainLayer);
+  trainLayer.addTo(map);
+  let popup = L.popup({
+      "autoClose": false,
+      "closeOnClock": null
+  }).setContent((`<b>Train No. </b> ${trainNumber}<br>
+            <b>Next Stop: </b> ${item.nextstop} <br>
+            <b>Line: </b> ${item.line}<br>
+            <b> Destination: </b> ${item.dest}`));
+  trainMarker.bindPopup(popup);
 }
