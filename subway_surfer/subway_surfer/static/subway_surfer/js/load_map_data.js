ddab176936f86b5ac69354ef052bfb9877f21d1b
d0ugherty <tdougherty16@gmail.com>
2024-02-05 21:21:59 -0500

    train locations are being displayed on the map

--- subway_surfer/subway_surfer/static/subway_surfer/js/load_map_data.js
+++ subway_surfer/subway_surfer/static/subway_surfer/js/load_map_data.js
@@ -1,29 +1,40 @@
-
+/***
+ * Loads and displays map data
+ * 
+ * 
+ */
 
 function load_loc_data(element_id){
     document.addEventListener('DOMContentLoaded', function() {
       var trainLocData = JSON.parse(document.getElementById(element_id).textContent);
-      //return trainLocData;
       displayLocation(trainLocData);
     });
 }
 
 function displayLocation(data){
-  data.forEach(element => console.log(element));
+  // Check if data itself is an array
+  Object.keys(data).forEach(key => {
+    let train = data[key];
+    console.log('train number: ${key}', train);
+    console.log('train number: ${key}', train.lat);
+    displayTrainCurrentLoc(train, trainLayer);
+  });
+
+  
 }
 
-function displayTrainCurrentLoc(item, trainLayer, icon) {
+
+function displayTrainCurrentLoc(item, trainLayer) {
   let trainNumber = item.trainno;
-  let trainMarker = L.marker([item.lat, item.lon], {
-      icon: icon
-  }).addTo(trainLayer);
+  let trainMarker = L.marker([item.lat, item.lon]).addTo(trainLayer);
   trainLayer.addTo(map);
   let popup = L.popup({
       "autoClose": false,
       "closeOnClock": null
-  }).setContent((`<b>Train No. </b> ${trainNumber}<br>
-            <b>Next Stop: </b> ${item.nextstop} <br>
-            <b>Line: </b> ${item.line}<br>
-            <b> Destination: </b> ${item.dest}`));
+  }).setContent((
+        `<b>Train No. </b> ${trainNumber}<br>
+        <b>Next Stop: </b> ${item.nextstop} <br>
+        <b>Line: </b> ${item.line}<br>
+        <b> Destination: </b> ${item.dest}`));
   trainMarker.bindPopup(popup);
 }
--- subway_surfer/subway_surfer/static/subway_surfer/js/map.js
+++ subway_surfer/subway_surfer/static/subway_surfer/js/map.js
@@ -5,31 +5,24 @@
  *   the map itself. This makes for easy marker removal either for toggling or 
  *   the retrieval of new data (live updating)
  * */
- const map = L.map('map', {
+
+const map = L.map('map', {
     center: [39.952325, -75.163705],
-   zoom: 11,
+    zoom: 11,
     layers: [
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
         L.tileLayer('https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png')
-      ],
-      tap: false
-  });
-  
-  map.attributionControl.addAttribution('Data <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'+ 
-                                      ' Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a>'+
-                                      '<a href="http://www.openrailwaymap.org/">OpenRailwayMap</a>');
-let trainLayer = L.layerGroup();
-let stationLayer = L.layerGroup().addTo(map);
-let trolleyLayer = L.layerGroup().addTo(map);
-let trolleyStopLayer = L.layerGroup().addTo(map);
+    ],
+        tap: false
+    });
+    
+    map.attributionControl.addAttribution('Data <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>'+ 
+                                        ' Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a>'+
+                                        '<a href="http://www.openrailwaymap.org/">OpenRailwayMap</a>');
 
-/**
-let trainIcon = L.icon({
-  iconUrl: './packages/leaflet/images/SEPTARegionalRail-bw.png',
-  iconSize: [30, 30]
-});
 
-let trolleyIcon = L.icon({
-  iconUrl: './packages/leaflet/images/SEPTATrolleytrans.png',
-  iconSize: [30, 30]
-});**/
+
+const trainLayer = L.layerGroup();
+const stationLayer = L.layerGroup().addTo(map);
+const trolleyLayer = L.layerGroup().addTo(map);
+const trolleyStopLayer = L.layerGroup().addTo(map);
