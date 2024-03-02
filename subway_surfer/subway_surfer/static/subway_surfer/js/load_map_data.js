12cb50a456403dfbfd25aa9ccdbe5ce5806ca5a1
d0ugherty <tdougherty16@gmail.com>
2024-03-02 16:07:34 -0500

    whitespace formatting

--- subway_surfer/subway_surfer/static/subway_surfer/js/load_map_data.js
+++ subway_surfer/subway_surfer/static/subway_surfer/js/load_map_data.js
@@ -22,7 +22,9 @@ setInterval(displayLocation, 5000);
 function displayTrainCurrentLoc(item, trainLayer) {
     let trainNumber = item.trainno;
     let trainMarker = L.marker([item.lat, item.lon]).addTo(trainLayer);
+    
     trainLayer.addTo(map);
+
     let popup = L.popup({
         "autoClose": false,
         "closeOnClock": null
@@ -31,16 +33,20 @@ function displayTrainCurrentLoc(item, trainLayer) {
             <b>Next Stop: </b> ${item.nextstop} <br>
             <b>Line: </b> ${item.line}<br>
             <b> Destination: </b> ${item.dest}`));
+
     trainMarker.bindPopup(popup);
 }
 
 function displayShapes(shape_data) {
     Object.keys(shape_data).forEach(key => {
+
         let latlngs = [];
         let shape = shape_data[key];
+
         for(const point of shape) {
             latlngs.push([point.shape_pt_lat, point.shape_pt_lon]);
         }
+
         L.polyline(latlngs, { color: '#43647c'}).addTo(map);
     });
 }
